55726ba27b6abff7787fe12bddb7f85303e62ba7
Erlang Parasu <erlangparasu@gmail.com>
2026-01-23 00:13:59 +0800

    wip: login usecase

--- features/home/domain/dtos/UserDTO.ts
+++
@@ -1,9 +0,0 @@
-// ● User will key in their name to proceed
-// ● User will be able to create a Todo
-// ● All Todos are able to be set done and delete Todo
-// ● User Todos are saved on local storage. Todo is saved by user name
-// ● Users can see their own Todo only
-export interface UserDTO {
-  id: number;
-  name: string;
-}
---
+++ features/login/domain/usecases/LoginUsecase.ts
@@ -0,0 +1,32 @@
+import { UserRepository } from "../repositories/UserRepository";
+
+export class LoginOrRegisterUsecase {
+    constructor(
+        private userRepository: UserRepository,
+    ) { }
+
+    async execute(params: { userName: string; }) {
+        const users = await this.userRepository.localGetAll();
+
+        const user = users.find((record) => { record.name === params.userName }) ?? null;
+        if (user) {
+            // NOTE: User already exists
+
+            return user;
+        } else {
+            // NOTE: Create new user
+
+            const maxId = users.reduce((max, user) => {
+                return user.id > max ? user.id : max;
+            }, 1);
+
+            const newUser = {
+                id: maxId + 1,
+                name: params.userName,
+            };
+            await this.userRepository.localAdd({ ...newUser });
+
+            return newUser;
+        }
+    }
+}

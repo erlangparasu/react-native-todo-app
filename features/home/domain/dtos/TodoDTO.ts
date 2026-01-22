c4029ab55c7df384c1ff42f43a53e1714455c413
Erlang Parasu <erlangparasu@gmail.com>
2026-01-22 23:00:47 +0800

    wip: domain dtos

---
+++ features/home/domain/dtos/TodoDTO.ts
@@ -0,0 +1,16 @@
+// ● User will key in their name to proceed
+// ● User will be able to create a Todo
+// ● All Todos are able to be set done and delete Todo
+// ● User Todos are saved on local storage. Todo is saved by user name
+// ● Users can see their own Todo only
+export interface TodoDTO {
+    id: number;
+    content: string;
+    dueDate: string;
+    status: "open" | "done" | "deleted";
+    createdAt: string;
+    doneAt: string;
+    updatedAt: string;
+    deletedAt: string;
+    userId: number;
+}

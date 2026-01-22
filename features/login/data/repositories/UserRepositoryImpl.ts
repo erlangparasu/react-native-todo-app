a2a7b0fbfc88faff7d61f44e00c9701a66bf1d03
Erlang Parasu <erlangparasu@gmail.com>
2026-01-23 00:11:56 +0800

    wip: user repo impl

---
+++ features/login/data/repositories/UserRepositoryImpl.ts
@@ -0,0 +1,47 @@
+import { UserDTO } from "../../domain/dtos/UserDTO";
+import { UserRepository } from "../../domain/repositories/UserRepository";
+import { UserLocalDatasource } from "../data-sources/UserLocalDatasource";
+
+export class UserRepositoryImpl implements UserRepository {
+  constructor(
+    private userLocalDatasource: UserLocalDatasource,
+  ) { }
+
+  async localGetAll(): Promise<UserDTO[]> {
+    const records = await this.userLocalDatasource.getList();
+
+    return records;
+  }
+
+  async localGetOne({ userId }: { userId: number }): Promise<UserDTO | null> {
+    const record = await this.userLocalDatasource.get({
+      userId: userId,
+    });
+
+    return record;
+  }
+
+  async localAdd(userItem: UserDTO): Promise<number> {
+    await this.userLocalDatasource.add({
+      userItem: userItem,
+    });
+
+    return userItem.id;
+  }
+
+  async localUpdate(userItem: UserDTO): Promise<boolean> {
+    await this.userLocalDatasource.update({
+      userItem: userItem,
+    });
+
+    return true;
+  }
+
+  async localDelete({ userId }: { userId: number }): Promise<boolean> {
+    await this.userLocalDatasource.delete({
+      userId: userId,
+    });
+
+    return true;
+  }
+}

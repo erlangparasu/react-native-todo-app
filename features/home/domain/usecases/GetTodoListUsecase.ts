d9c751db0e2e825dfd9db16022c5756c05538f4b
Erlang Parasu <erlangparasu@gmail.com>
2026-01-23 00:22:35 +0800

    wip: get todo list usecase

---
+++ features/home/domain/usecases/GetTodoListUsecase.ts
@@ -0,0 +1,20 @@
+import { TodoRepository } from "../repositories/TodoRepository";
+
+export class GetTodoListUsecase {
+    constructor(
+        private todoRepository: TodoRepository,
+    ) {}
+
+    async execute(params: { userId: number }) {
+        const todos = await this.todoRepository.localGetAll();
+
+        const userTodos = todos.filter((rec) => {
+            return rec.userId === params.userId;
+        });
+
+        return userTodos;
+    }
+}
+
+//   localAdd(todoItem: TodoDTO): Promise<number>;
+//   localUpdate(todoItem: TodoDTO): Promise<boolean>;

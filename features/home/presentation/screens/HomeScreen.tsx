c5dfb03b96bbbf749afc33d5b8d3662325467e81
Erlang Parasu <erlangparasu@gmail.com>
2026-01-23 03:32:38 +0800

    feat: ui todo Card component with props

--- features/home/presentation/screens/HomeScreen.tsx
+++ features/home/presentation/screens/HomeScreen.tsx
@@ -38,7 +38,7 @@ const HomeScreen = () => {
                 }}
               >
                 {"Hi, "}
-                {"ChangeMe"}
+                {"Erlang Parasu"}
               </Text>
             </View>
           </View>
@@ -50,111 +50,42 @@ const HomeScreen = () => {
               borderTopRightRadius: 16,
             }}
           >
-            <View
-              style={{
-                backgroundColor: "rgb(63,68,76)",
-                padding: 16 + 4,
-                margin: 16,
-                borderWidth: 0,
-                borderRadius: 12,
+            <TodoOneCard
+              id={1}
+              status="open"
+              title="Dummy1"
+              dueDate="2026-01-23"
+              onDeletePress={(_) => {
+                //
               }}
-            >
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "center",
-                }}
-              >
-                <View
-                  style={{
-                    flex: 1,
-                    flexDirection: "row",
-                  }}
-                >
-                  <OpenBadge />
-                  <DoneBadge />
-                  <OverdueBadge />
-                </View>
-
-                <DeleteButton
-                  onPress={() => {
-                    //
-                  }}
-                />
-              </View>
-
-              <Text
-                style={{
-                  color: "white",
-                  fontSize: 28,
-                  fontWeight: "bold",
-                  marginTop: 12,
-                }}
-              >
-                {"TitleHere"}
-              </Text>
-
-              <View
-                style={{
-                  flexDirection: "row",
-                }}
-              >
-                <View
-                  style={{
-                    flex: 1,
-                  }}
-                >
-                  <Text
-                    style={{
-                      color: "white",
-                      fontSize: 16,
-                      marginTop: 16,
-                    }}
-                  >
-                    {"Due Date:"}
-                  </Text>
-                  <Text
-                    style={{
-                      color: "white",
-                      fontSize: 16,
-                      fontWeight: "bold",
-                    }}
-                  >
-                    {"DateHere"}
-                  </Text>
-                </View>
-                <View
-                  style={{
-                    justifyContent: "flex-end",
-                  }}
-                >
-                  <DoneButton
-                    onPress={() => {
-                      //
-                    }}
-                  />
-                </View>
-              </View>
-            </View>
-
-            <View
-              style={{
-                marginTop: 32,
-                marginLeft: 32,
-                marginRight: 32,
+              onDonePress={(_) => {
+                //
               }}
-            >
-              <Text
-                style={{
-                  color: "white",
-                  fontSize: 4 * 9,
-                  fontWeight: "bold",
-                }}
-              >
-                {"Hi, "}
-                {"Erlang Parasu"}
-              </Text>
-            </View>
+            />
+            <TodoOneCard
+              id={2}
+              status="done"
+              title="Dummy2"
+              dueDate="2026-01-23"
+              onDeletePress={(_) => {
+                //
+              }}
+              onDonePress={(_) => {
+                //
+              }}
+            />
+            <TodoOneCard
+              id={3}
+              status="overdue"
+              title="Dummy3"
+              dueDate="2026-01-23"
+              onDeletePress={(_) => {
+                //
+              }}
+              onDonePress={(_) => {
+                //
+              }}
+            />
           </ScrollView>
         </View>
       </SafeAreaView>
@@ -163,6 +94,113 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+const TodoOneCard = (props: {
+  id: number;
+  status: "open" | "done" | "overdue";
+  title: string;
+  dueDate: string;
+  onDeletePress: (id: number) => void;
+  onDonePress: (id: number) => void;
+}) => {
+  return (
+    <View
+      style={{
+        backgroundColor: "rgb(63,68,76)",
+        padding: 16 + 4,
+        margin: 16,
+        borderWidth: 0,
+        borderRadius: 12,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+          }}
+        >
+          {(() => {
+            if (props.status === "open") {
+              return <OpenBadge />;
+            }
+            if (props.status === "done") {
+              return <DoneBadge />;
+            }
+            if (props.status === "overdue") {
+              return <OverdueBadge />;
+            }
+            return <></>;
+          })()}
+        </View>
+
+        <DeleteButton
+          onPress={() => {
+            props.onDeletePress(props.id);
+          }}
+        />
+      </View>
+
+      <Text
+        style={{
+          color: "white",
+          fontSize: 28,
+          fontWeight: "bold",
+          marginTop: 12,
+        }}
+      >
+        {props.title}
+      </Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 16,
+              marginTop: 16,
+            }}
+          >
+            {"Due Date:"}
+          </Text>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 16,
+              fontWeight: "bold",
+            }}
+          >
+            {props.dueDate}
+          </Text>
+        </View>
+        <View
+          style={{
+            justifyContent: "flex-end",
+          }}
+        >
+          <DoneButton
+            onPress={() => {
+              props.onDonePress(props.id);
+            }}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
 
 export function OpenBadge() {
   return (

4b5a319f2752a8b2fbfcbc16e7e88dcffd617329
Erlang Parasu <erlangparasu@gmail.com>
2026-01-23 00:52:41 +0800

    refactor: reformat

--- features/home/domain/usecases/DoneTodoUsecase.ts
+++ features/home/domain/usecases/DoneTodoUsecase.ts
@@ -2,42 +2,42 @@ import { TodoDTO } from "../dtos/TodoDTO";
 import { TodoRepository } from "../repositories/TodoRepository";
 
 export class DoneTodoUsecase {
-    constructor(
-        private todoRepository: TodoRepository,
-    ) {}
+  constructor(
+    private todoRepository: TodoRepository,
+  ) {}
 
-    async execute(params: {
-        userId: number;
-        todoId: number;
-    }): Promise<boolean> {
-        const todos = await this.todoRepository.localGetAll();
+  async execute(params: {
+    userId: number;
+    todoId: number;
+  }): Promise<boolean> {
+    const todos = await this.todoRepository.localGetAll();
 
-        const todo = todos.find((rec) => {
-            return rec.id === params.todoId;
-        });
+    const todo = todos.find((rec) => {
+      return rec.id === params.todoId;
+    });
 
-        if (todo) {
-            // ok
-        } else {
-            return false;
-        }
-
-        if (todo.userId !== params.userId) {
-            // NOTE: Forbidden
-            return false;
-        }
-
-        const now = (new Date()).toISOString();
-
-        const newTodo: TodoDTO = {
-            ...todo,
-            status: "done",
-            doneAt: now,
-            updatedAt: now,
-            deletedAt: null,
-        };
-        await this.todoRepository.localUpdate({ ...newTodo });
-
-        return true;
+    if (todo) {
+      // ok
+    } else {
+      return false;
     }
+
+    if (todo.userId !== params.userId) {
+      // NOTE: Forbidden
+      return false;
+    }
+
+    const now = (new Date()).toISOString();
+
+    const newTodo: TodoDTO = {
+      ...todo,
+      status: "done",
+      doneAt: now,
+      updatedAt: now,
+      deletedAt: null,
+    };
+    await this.todoRepository.localUpdate({ ...newTodo });
+
+    return true;
+  }
 }
--- features/home/domain/usecases/GetTodoListUsecase.ts
+++ features/home/domain/usecases/GetTodoListUsecase.ts
@@ -2,21 +2,21 @@ import { TodoDTO } from "../dtos/TodoDTO";
 import { TodoRepository } from "../repositories/TodoRepository";
 
 export class GetTodoListUsecase {
-    constructor(
-        private todoRepository: TodoRepository,
-    ) {}
+  constructor(
+    private todoRepository: TodoRepository,
+  ) {}
 
-    async execute(params: {
-        userId: number;
-    }): Promise<TodoDTO[]> {
-        const todos = await this.todoRepository.localGetAll();
+  async execute(params: {
+    userId: number;
+  }): Promise<TodoDTO[]> {
+    const todos = await this.todoRepository.localGetAll();
 
-        const userTodos = todos.filter((rec) => {
-            return rec.userId === params.userId;
-        });
+    const userTodos = todos.filter((rec) => {
+      return rec.userId === params.userId;
+    });
 
-        return userTodos;
-    }
+    return userTodos;
+  }
 }
 
 //   localAdd(todoItem: TodoDTO): Promise<number>;
